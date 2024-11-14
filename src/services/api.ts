0e5310db90c41244745990b7c06347378f4bcf11
anderson16_ <andersonaraujoc1@gmail.com>
2024-11-14 13:30:07 -0300

    fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.MODE === "development" ? "/api" : "/api/proxy";
+const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 export const api = axios.create({
   baseURL: baseURL,
