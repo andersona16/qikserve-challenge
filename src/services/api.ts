dfabc6b5eef340be001bb601d52f123d439c0089
anderson16_ <andersonaraujoc1@gmail.com>
2024-11-14 12:53:58 -0300

    fit deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,17 @@
 import axios from "axios";
 
+const baseURL = "https://cdn-dev.preoday.com/challenge";
+
 export const api = axios.create({
-  baseURL: "/challenge",
+  baseURL: baseURL,
 });
 
 export const fetchRestaurantData = async () => {
   const response = await api.get("venue/9");
+  return response.data;
+};
+
+export const fetchRestaurantMenu = async () => {
+  const response = await api.get("menu");
   return response.data;
 };
